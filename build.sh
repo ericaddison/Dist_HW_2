b5963d8408aa2416851b45abd21e5b6c32563bda
Eric Addison <ericaddison@gmail.com>
2017-10-11 10:51:49 -0500

    Moved source back into src

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir build
-javac -classpath ./lib/jackson-annotations-2.9.1.jar:./lib/jackson-databind-2.9.1.jar:./lib/jackson-core-2.9.1.jar:. -d ./build/ *.java
+javac -classpath ./lib/jackson-annotations-2.9.1.jar:./lib/jackson-databind-2.9.1.jar:./lib/jackson-core-2.9.1.jar:. -d ./build/ src/*.java
 
 cd build
 jar cvf ../HW2.jar *
